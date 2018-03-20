3c65bbee0557b95f6df224696711271fd4720296
yangjw <yangjw.kai7@gmail.com>
2018-03-20 14:41:35 +0800

    初始化项目

---
+++ ModuleLib/ModuleLib/ModuleLib.h
@@ -0,0 +1,19 @@
+//
+//  ModuleLib.h
+//  ModuleLib
+//
+//  Created by yangjw on 2018/3/20.
+//  Copyright © 2018年 yangjw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ModuleLib.
+FOUNDATION_EXPORT double ModuleLibVersionNumber;
+
+//! Project version string for ModuleLib.
+FOUNDATION_EXPORT const unsigned char ModuleLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModuleLib/PublicHeader.h>
+
+
